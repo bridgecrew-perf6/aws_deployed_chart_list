01b3f7de107aaa2ea1d2f9dbb565cb5cfb271306
Amna <amna.bashir2@gmail.com>
2022-03-16 14:53:36 +0000

    name changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const genres = [
     {name: "All", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"},
     {name: "Rock", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json"},
-    {name: "Dance", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json"},
+    {name: "Vibrant", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json"},
     {name: "Country", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json"}
   ]
   
